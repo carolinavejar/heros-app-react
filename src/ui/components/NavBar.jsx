f0e6ab91214c27e9203e340b7fda1b09d60695c9
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-24 19:20:04 -0300

    Navigate push replace - Use Navigate

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+    const navigate = useNavigate();
+
+    const onLogout = ()=> {
+        navigate('/login', {
+            replace: true
+        })
+        console.log("logout");
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -36,7 +45,8 @@ export const Navbar = () => {
                     <span className='nav-item nav-link text-info'>
                         Carito
                     </span>
-                    <button className='nav-item btn nav-link'>
+                    <button className='nav-item btn nav-link'
+                    onClick={ onLogout }>
                         Logout
                     </button>
                 </ul>
